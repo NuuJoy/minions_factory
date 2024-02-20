38fc1753296644fb9a4a5ad47ec46b28048eca65
NuuJoy <halala_two@hotmail.com>
2024-02-20 20:42:23 +0700

    Add login API
    - Add PyJWT lib to requirement
    - Add route /login for user to get JWT
    - Change separator of secret files

--- minifac_accounts/accounts.py
+++ minifac_accounts/accounts.py
@@ -1,9 +1,15 @@
 
 
 import os
+import datetime
 
-from flask import Flask, make_response
+from flask import Flask, request
 import mysql.connector
+import jwt
+
+
+with open('/run/secrets/accounts_auth', 'r') as rfl:
+    JWT_ALGORITHM, JWT_SECRET = rfl.read().split()
 
 
 class MySQL_Connect():
@@ -12,7 +18,7 @@ class MySQL_Connect():
         self.port = os.getenv('MYSQL_PORT')
         self.dbname = os.getenv('MYSQL_DBNAME')
         with open('/run/secrets/mysql_auth', 'r') as rfl:
-            self.user, self.pswd = rfl.read().split(',')
+            self.user, self.pswd = rfl.read().split()
 
     def __enter__(self):
         self._conn = mysql.connector.connect(
@@ -36,11 +42,67 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def index():
-    with mysql_connect as conn:
-        with conn.cursor() as curs:
-            # check database connection
-            curs.execute('''SELECT * FROM minifac_db.accounts;''')
-            curs.fetchall()
-    response = make_response('Ready!', 200)
-    response.mimetype = "text/plain"
-    return response
+    return {
+        'status': 'success',
+        'message': 'welcome to accounts api'
+    }, 200
+
+
+@app.route('/login', methods=['POST'])
+def login():
+    auth = request.authorization
+
+    if (auth is None) or (auth.username is None) or (auth.password is None):
+        return {
+            'status': 'fail',
+            'message': 'invalid authorization attribute'
+        }, 400
+
+    try:
+        with mysql_connect as conn:
+            with conn.cursor() as curs:
+                # check database connection
+                curs.execute(
+                    f'''
+                    SELECT
+                        username, password
+                    FROM
+                        minifac_db.accounts
+                    WHERE
+                        username = '{auth.username}'
+                    ;''')
+                resp = curs.fetchall()
+    except Exception:
+        return {
+            'status': 'fail',
+            'message': 'bad database connection'
+        }, 500
+
+    if (len(resp) == 1) and (len(resp[0]) == 2):
+        db_username, db_password = resp[0]
+    else:
+        return {
+            'status': 'fail',
+            'message': 'invalid username or password'
+        }, 401
+
+    if (db_username == auth.username) and (db_password == auth.password):
+        iat = datetime.datetime.utcnow()
+        exp = iat + datetime.timedelta(hours=12)
+        auth_token = jwt.encode(
+            {
+                'sub': auth.username,
+                'exp': exp.timestamp()
+            },
+            JWT_SECRET,
+            algorithm=JWT_ALGORITHM
+        )
+        return {
+            'status': 'success',
+            'message': 'user login successfully',
+            'auth_token': auth_token}, 200
+    else:
+        return {
+            'status': 'fail',
+            'message': 'invalid username or password'
+        }, 401
