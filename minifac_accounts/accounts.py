088f06c46db42fb78ba4a5e1852b04636322edf8
NuuJoy <halala_two@hotmail.com>
2024-02-21 15:08:17 +0700

    Add charge end-point
    - Add charge end-point for charging to user credit
    - Import Decimal for credit calculation

--- minifac_accounts/accounts.py
+++ minifac_accounts/accounts.py
@@ -3,6 +3,7 @@
 import os
 import datetime
 from threading import Lock
+from decimal import Decimal
 
 from flask import Flask, request
 import mysql.connector
@@ -168,5 +169,50 @@ def get_account_info():
                 resp = curs.fetchone()
         name, credit = resp
         return {'name': name, 'credit': credit}, 200
+    else:
+        return validation, statuscode
+
+
+@app.route('/charge', methods=['PATCH'])
+def charge():
+    amount = Decimal(request.args.to_dict()['amount'])
+    if amount <= 0:
+        return {
+            'status': 'fail',
+            'message': 'not support negative amount value'
+        }, 400
+
+    validation, statuscode = validate()
+    if validation['status'] == 'success':
+        username = validation['claims']['sub']
+        with mysql_connect as conn:
+            with conn.cursor() as curs:
+                curs.execute(
+                    f'''
+                    SELECT Credit
+                    FROM minifac_db.accounts
+                    WHERE username = '{username}'
+                    ;'''
+                )
+                credit = Decimal(curs.fetchone()[0])
+                if credit >= amount:
+                    newcredit = credit - amount
+                    curs.execute(
+                        f'''
+                        UPDATE minifac_db.accounts
+                        SET Credit = {newcredit}
+                        WHERE username = '{username}'
+                        ;
+                        COMMIT;'''
+                    )
+                    return {
+                        'status': 'success',
+                        'message': 'purchase successfully',
+                    }, 200
+                else:
+                    return {
+                        'status': 'fail',
+                        'message': 'not enough credit',
+                    }, 409
     else:
         return validation, statuscode
