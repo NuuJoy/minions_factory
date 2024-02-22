6fdc7166f553d48483f5a2c07df06bd8dbf792fb
NuuJoy <halala_two@hotmail.com>
2024-02-22 13:24:31 +0700

    Refactor: "validate" to decorator
    Change "validate" function to work as decorator

--- minifac_accounts/accounts.py
+++ minifac_accounts/accounts.py
@@ -4,6 +4,7 @@ import os
 import datetime
 from threading import Lock
 from decimal import Decimal
+from functools import wraps
 
 from flask import Flask, request
 import mysql.connector
@@ -112,115 +113,113 @@ def login():
         }, 401
 
 
+def with_validation(func):
+    @wraps(func)
+    def decorated_function(*args, **kwargs):
+        auth = request.authorization
+        if (auth is None) or (auth.type != 'bearer') or (auth.token is None):
+            return {
+                'status': 'fail',
+                'message': 'invalid authorization attribute'
+            }, 400
+        try:
+            claims = jwt.decode(
+                auth.token,
+                JWT_SECRET,
+                algorithms=JWT_ALGORITHM
+            )
+        except InvalidSignatureError:
+            return {
+                'status': 'fail',
+                'message': 'invalid token'
+            }, 401
+
+        if datetime.datetime.utcnow().timestamp() <= claims['exp']:
+            return func(claims)
+        else:
+            return {
+                'status': 'fail',
+                'message': 'token expired'
+            }, 401
+    return decorated_function
+
+
 @app.route('/validate', methods=['GET'])
-def validate():
-    auth = request.authorization
-
-    if (auth is None) or (auth.type != 'bearer') or (auth.token is None):
-        return {
-            'status': 'fail',
-            'message': 'invalid authorization attribute'
-        }, 400
-
-    try:
-        claims = jwt.decode(
-            auth.token,
-            JWT_SECRET,
-            algorithms=JWT_ALGORITHM
-        )
-    except InvalidSignatureError:
-        return {
-            'status': 'fail',
-            'message': 'invalid token'
-        }, 401
-
-    if datetime.datetime.utcnow().timestamp() <= claims['exp']:
-        return {
-            'status': 'success',
-            'message': f'welcome {claims["sub"]}',
-            'claims': claims
-        }, 200
-    else:
-        return {
-            'status': 'fail',
-            'message': 'token expired'
-        }, 401
+@with_validation
+def validate(claims):
+    return {
+        'status': 'success',
+        'message': f'token valid: {claims}'
+    }, 200
 
 
 @app.route('/info', methods=['GET'])
-def info():
-    validation, statuscode = validate()
-    if validation['status'] == 'success':
-        username = validation['claims']['sub']
-        keymap = {
-            'name': 'CustomerName',
-            'credit': 'Credit'
-        }
-        request_cols = []
-        try:
-            for col in request.args.to_dict()['cols'].split(','):
-                if col in keymap:
-                    request_cols.append(keymap[col])
-        except Exception:
-            pass
-        if not request_cols:
-            return {}, 200
+@with_validation
+def info(claims):
+    username = claims['sub']
+    keymap = {
+        'name': 'CustomerName',
+        'credit': 'Credit'
+    }
+    request_cols = []
+    try:
+        for col in request.args.to_dict()['cols'].split(','):
+            if col in keymap:
+                request_cols.append(keymap[col])
+    except Exception:
+        pass
+    if not request_cols:
+        return {}, 200
 
-        with mysql_connect as conn:
-            with conn.cursor() as curs:
-                curs.execute(
-                    f'''
-                    SELECT {','.join(request_cols)}
-                    FROM minifac_db.accounts
-                    WHERE username = '{username}'
-                    ;''')
-                resp = curs.fetchone()
-        name, credit = resp
-        return {'name': name, 'credit': credit}, 200
-    else:
-        return validation, statuscode
+    with mysql_connect as conn:
+        with conn.cursor() as curs:
+            curs.execute(
+                f'''
+                SELECT {','.join(request_cols)}
+                FROM minifac_db.accounts
+                WHERE username = '{username}'
+                ;''')
+            resp = curs.fetchone()
+    name, credit = resp
+    return {'name': name, 'credit': credit}, 200
 
 
 @app.route('/charge', methods=['PATCH'])
-def charge():
+@with_validation
+def charge(claims):
     amount = Decimal(request.args.to_dict()['amount'])
     if amount <= 0:
         return {
             'status': 'fail',
             'message': 'not support negative amount value'
         }, 400
-
-    validation, statuscode = validate()
-    if validation['status'] == 'success':
-        username = validation['claims']['sub']
-        with mysql_connect as conn:
-            with conn.cursor() as curs:
+    username = claims['sub']
+    with mysql_connect as conn:
+        with conn.cursor() as curs:
+            curs.execute(
+                f'''
+                SELECT Credit
+                FROM minifac_db.accounts
+                WHERE username = '{username}'
+                ;'''
+            )
+            credit = Decimal(curs.fetchone()[0])
+            if credit >= amount:
+                newcredit = credit - amount
                 curs.execute(
                     f'''
-                    SELECT Credit
-                    FROM minifac_db.accounts
+                    UPDATE minifac_db.accounts
+                    SET Credit = {newcredit}
                     WHERE username = '{username}'
-                    ;'''
+                    ;
+                    COMMIT;'''
                 )
-                credit = Decimal(curs.fetchone()[0])
-                if credit >= amount:
-                    newcredit = credit - amount
-                    curs.execute(
-                        f'''
-                        UPDATE minifac_db.accounts
-                        SET Credit = {newcredit}
-                        WHERE username = '{username}'
-                        ;
-                        COMMIT;'''
-                    )
-                    return {
-                        'status': 'success',
-                        'message': 'purchase successfully',
-                    }, 200
-                else:
-                    return {
-                        'status': 'fail',
-                        'message': 'not enough credit',
-                    }, 409
-    else:
-        return validation, statuscode
+                return {
+                    'status': 'success',
+                    'message': 'purchase successfully',
+                }, 200
+            else:
+                return {
+                    'status': 'fail',
+                    'message': 'not enough credit',
+                }, 409
