da7b05988a79880d5a4480a6d53a939ec60ae3af
NuuJoy <halala_two@hotmail.com>
2024-02-25 10:08:55 +0700

    Add login page
    - Add login page for user to fill-in
    - Use home page as validate page and also redirect to login if validation is not success
    - Remove validate end-point (use home instead)

--- minifac_authentication/authentication.py
+++ minifac_authentication/authentication.py
@@ -3,7 +3,7 @@
 import os
 import datetime
 
-from flask import Flask, request, make_response
+from flask import Flask, request, make_response, render_template, redirect
 
 import jwt
 
@@ -23,80 +23,74 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def index():
-    return {
-        'status': 'success',
-        'message': 'welcome to authentication api'
-    }, 200
+    body, status = with_validation(lambda claims: (claims, 200))()
+    if status == 200:
+        return f'Welcome {body["sub"]} !!!', status
+    else:
+        error = body['message']
+        return redirect(f'/login?error={error}')
 
 
-@app.route('/login', methods=['POST'])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    auth = request.authorization
-
-    if (auth is None) or (auth.username is None) or (auth.password is None):
-        return {
-            'status': 'fail',
-            'message': 'invalid authorization attribute'
-        }, 400
-
-    try:
-        with mysql_connect as conn:
-            with conn.cursor() as curs:
-                # check database connection
-                curs.execute(
-                    f'''
-                    SELECT username, password
-                    FROM minifac_db.accounts
-                    WHERE username = '{auth.username}'
-                    ;''')
-                resp = curs.fetchone()
-    except Exception:
-        return {
-            'status': 'fail',
-            'message': 'bad database connection'
-        }, 500
-
-    if resp:
-        db_username, db_password = resp
+    if 'error' in (args := request.args.to_dict()):
+        error = args['error']
     else:
-        return {
-            'status': 'fail',
-            'message': 'invalid username or password'
-        }, 401
+        error = None
 
-    if (db_username == auth.username) and (db_password == auth.password):
-        iat = datetime.datetime.utcnow()
-        exp = iat + datetime.timedelta(hours=12)
-        resp = make_response(
-            {
-                'status': 'success',
-                'message': 'user login successfully'
-            }, 200
-        )
-        resp.set_cookie(
-            'token',
-            jwt.encode(
-                {
-                    'sub': auth.username,
-                    'exp': exp.timestamp()
-                },
-                key=os.environ['JWT_SECRET'],
-                algorithm=os.environ['JWT_ALGORITHM']
-            ),
-            httponly=True
-        )
-        return resp
+    if request.method == 'GET':
+        return render_template('login.html', error=error)
     else:
-        return {
-            'status': 'fail',
-            'message': 'invalid username or password'
-        }, 401
+        username = None
+        password = None
+        try:
+            username = request.authorization.username
+            password = request.authorization.password
+        except Exception:
+            try:
+                username = request.form['username']
+                password = request.form['password']
+            except Exception:
+                pass
 
+        if (username is None) or (password is None):
+            return redirect('/login?error=invalid login method')
 
-@app.route('/validate', methods=['GET'])
-@with_validation
-def validate(claims):
-    return {
-        'status': 'success',
-        'message': f'token valid: {claims}'
-    }, 200
+        try:
+            with mysql_connect as conn:
+                with conn.cursor() as curs:
+                    # check database connection
+                    curs.execute(
+                        f'''
+                        SELECT username, password
+                        FROM minifac_db.accounts
+                        WHERE username = '{username}'
+                        ;''')
+                    resp = curs.fetchone()
+        except Exception:
+            return redirect('/login?error=bad database connection')
+
+        if resp:
+            db_username, db_password = resp
+        else:
+            return redirect('/login?error=invalid username or password')
+
+        if (db_username == username) and (db_password == password):
+            iat = datetime.datetime.utcnow()
+            exp = iat + datetime.timedelta(minutes=5)
+            resp = make_response(redirect('/'))
+            resp.set_cookie(
+                'token',
+                jwt.encode(
+                    {
+                        'sub': username,
+                        'exp': exp.timestamp()
+                    },
+                    key=os.environ['JWT_SECRET'],
+                    algorithm=os.environ['JWT_ALGORITHM']
+                ),
+                httponly=True
+            )
+            return resp
+        else:
+            return redirect('/login?error=invalid username or password')
