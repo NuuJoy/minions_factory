ce3baec2b8d3d5f0584316d59c673ef16155cf6b
NuuJoy <halala_two@hotmail.com>
2024-02-27 21:25:11 +0700

    Add shopping card
    - Show list of added unit
    - Show total price for all units

--- minifac_customer/static/js/customer_purchase_order.js
+++ minifac_customer/static/js/customer_purchase_order.js
@@ -159,22 +159,123 @@ class Store_Items_Layout_Class {
 }
 
 
+class Shopping_Cart_Class {
+    constructor (shopping_cart_layout_id) {
+        this.layout = document.getElementById(shopping_cart_layout_id)
+        this.header_elem = document.createElement('h2')
+        this.layout.appendChild(this.header_elem)
+        this.table = document.createElement('table')
+        this.layout.appendChild(this.table)
+        this.review_button = document.createElement('button')
+        this.review_button.textContent = 'Review purchase'
+        this.review_button.disabled = true
+        this.layout.appendChild(this.review_button)
+
+        this.cart_items = []
+    }
+
+    set_header (header) {
+        this.header_elem.innerHTML = header
+    }
+
+    clear_items () {
+        this.cart_items = []
+        this.table.replaceChildren()
+        this.review_button.disabled = true
+    }
+
+    add_unit (head_values, body_values, arms_values, legs_values) {
+        let inputs = [head_values, body_values, arms_values, legs_values]
+        if (this.table.childElementCount == 0) {
+            let header_row = document.createElement('tr')
+            this.table.appendChild(header_row)
+            let col
+            for (let val of ['Head', 'Body', 'Arms', 'Legs', 'Price']) {
+                col = document.createElement('th')
+                col.textContent = val
+                header_row.appendChild(col)
+            }
+        }
+        let data_row = document.createElement('tr')
+        this.table.appendChild(data_row)
+        let vals
+        let col
+        let sum_price = 0
+        let unit_items = []
+        for (let values of inputs) {
+            vals = values.split(',')
+            unit_items.push(Number(vals[0]))
+            col = document.createElement('td')
+            col.textContent = vals[2]
+            sum_price += Number(vals[3])
+            data_row.appendChild(col)
+        }
+        col = document.createElement('td')
+        col.textContent = sum_price
+        data_row.appendChild(col)
+
+        this.cart_items.push(unit_items)
+        this.review_button.disabled = false
+    }
+}
+
+
 var user_info_layout = new User_Info_Layout_Class('user_info_layout')
 user_info_layout.set_header('Customer infomations')
 
 var store_items_layout = new Store_Items_Layout_Class('store_items_layout')
 store_items_layout.set_header('All items in our store')
 
+var shopping_cart = new Shopping_Cart_Class('shopping_cart')
+shopping_cart.set_header('Shopping Cart')
 
 function calculate_sum_price() {
     store_items_layout.calculate_price()
 }
 
 
-for (let selector of store_items_layout.selectors) {
-    console.log('selector: ', selector)
-    selector.addEventListener("change", calculate_sum_price);
+function add_items_to_cart() {
+    let selected_values = []
+    for (let selector of store_items_layout.selectors) {
+        selected_values.push(selector.value)
+    }
+    shopping_cart.add_unit(...selected_values)
 }
+
+
+function goto_review_purchase() {
+    let form = document.createElement('form')
+    form.method = 'post'
+    form.action = window.location.origin + '/review_purchase'
+
+    let hiddenField = document.createElement('input')
+    hiddenField.type = 'hidden'
+    hiddenField.name = 'order'
+    hiddenField.value = JSON.stringify(shopping_cart.cart_items)
+    form.appendChild(hiddenField)
+
+    document.body.appendChild(form)
+    form.submit()
+}
+
+
+// selector event
+for (let selector of store_items_layout.selectors) {
+    selector.addEventListener(
+        "change", calculate_sum_price
+    )
+}
+
+// add-to-cart button event
+store_items_layout.add_to_cart_button.addEventListener(
+    "click", add_items_to_cart
+)
+
+
+// review-purchase button event
+shopping_cart.review_button.addEventListener(
+    "click", goto_review_purchase
+)
 
 
 function refresh_page() {
@@ -185,6 +286,8 @@ function refresh_page() {
 
     store_items_layout.clear_items()
     store_items_layout.add_items(page_info['allitems'])
+
+    shopping_cart.clear_items()
 }
 
 
